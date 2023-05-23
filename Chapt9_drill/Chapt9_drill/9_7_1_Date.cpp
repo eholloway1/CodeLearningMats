7064d93453a44f17f6d1e6606eb8ae0f6f38341a
Evan Holloway <eholloway00@gmail.com>
2023-05-22 17:56:32 -0700

    Add files via upload

--- Chapt9_drill/Chapt9_drill/9_7_1_Date.cpp
+++ Chapt9_drill/Chapt9_drill/9_7_1_Date.cpp
@@ -1,27 +1,34 @@
 #include "../../std_lib_facilities.h"
 /*
-	Drill 9.3
+	Drill 9.5
 
 	Evan Holloway	May 22, 2023
 
-	Get chapt 9.4.3 date to run. test with date June 25, 1978 and one bad date.
+	Get chapt 9.7.1 date to run. test with date June 25, 1978 and one bad date.
 	Date check can be simple and ingore leap years. Check for month in [1,12] range and date in [1,31]
 */
-class Date {
-	int y, m, d;
+enum class Month {
+	jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
 
+class Date {
 	public:
-		Date(int y, int m, int d)
+		Date(int y, Month m, int d)	
 			:y{ y }, m{ m }, d{ d }
 		{
-			if(m < 1 || m > 12) error("Please enter a valid month of year");
-			if(d < 1 || d > 31) error("Please enter a valid day of month");
+			if (int(m) < 1 || int(m) > 12) error("Please enter a valid month of year");
+			if (d < 1 || d > 31) error("Please enter a valid day of month");
 		}
 
 		void add_day(int);
-		int month() const {return m;}
-		int day() const {return d;}
-		int year() const {return y;}
+		int month() const { return int(m); }
+		int day() const { return d; }
+		int year() const { return y; }
+
+	private:
+		int y;
+		Month m;
+		int d;
 };
 
 ostream& operator<<(ostream& os, const Date& d) {
@@ -30,12 +37,12 @@ ostream& operator<<(ostream& os, const Date& d) {
 
 int main()
 try {
-	Date today{1978, 6, 25};
-	Date tomorrow{1978, 6, today.day() + 1};
+	Date today{ 1978, Month::jun, 25 };
+	Date tomorrow{ 1978, Month(today.month()), today.day() + 1};
 
 	cout << today << " " << tomorrow << '\n';
-	
-	Date Bad_day{1900, 13, 42};
+
+	Date Bad_day{ 1900, Month(14), 42};
 
 	cout << Bad_day << '\n';
 
@@ -45,12 +52,12 @@ try {
 catch (exception& e) {
 	cerr << e.what() << '\n';
 	keep_window_open();
-	return 0;
+	return 1;
 }
 catch (...) {
 	cerr << "Uknown exception\n";
 	keep_window_open();
-	return 1;
+	return 2;
 }
 
 void Date::add_day(int n) {
---
+++ Chapt9_drill/Chapt9_drill/9_7_4_Date.cpp
@@ -0,0 +1,68 @@
+#include "../../std_lib_facilities.h"
+/*
+	Drill 9.5
+
+	Evan Holloway	May 22, 2023
+
+	Get chapt 9.7.4 date to run. test with date June 25, 1978 and one bad date.
+	Date check can be simple and ingore leap years. Check for month in [1,12] range and date in [1,31]
+	
+	const member functions are the addition in 9.7.4 which I added earlier to make the ostream 
+	<< overload function
+*/
+enum class Month {
+	jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
+
+class Date {
+public:
+	Date(int y, Month m, int d)
+		:y{ y }, m{ m }, d{ d }
+	{
+		if (int(m) < 1 || int(m) > 12) error("Please enter a valid month of year");
+		if (d < 1 || d > 31) error("Please enter a valid day of month");
+	}
+
+	void add_day(int);
+	int month() const { return int(m); }
+	int day() const { return d; }
+	int year() const { return y; }
+
+private:
+	int y;
+	Month m;
+	int d;
+};
+
+ostream& operator<<(ostream& os, const Date& d) {
+	return os << '(' << d.day() << ',' << d.month() << ',' << d.year() << ')';
+}
+
+int main()
+try {
+	Date today{ 1978, Month::jun, 25 };
+	Date tomorrow{ 1978, Month(today.month()), today.day() + 1 };
+
+	cout << today << " " << tomorrow << '\n';
+
+	Date Bad_day{ 1900, Month(14), 42 };
+
+	cout << Bad_day << '\n';
+
+	keep_window_open();
+	return 0;
+}
+catch (exception& e) {
+	cerr << e.what() << '\n';
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "Uknown exception\n";
+	keep_window_open();
+	return 2;
+}
+
+void Date::add_day(int n) {
+	this->d += n;
+}
